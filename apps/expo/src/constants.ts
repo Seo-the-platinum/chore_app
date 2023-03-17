10f027df96015433107c225b8698a2db7899b90a
Eliseo Flores <eliseo_flores530@yahoo.com>
2023-03-16 21:29:38 -0700

    added google to list of providers and entered client_id and secret. also connected google with clerk

---
+++ apps/expo/src/constants.ts
@@ -0,0 +1,2 @@
+export const CLERK_PUBLISHER_KEY =
+  "pk_test_c3Ryb25nLWhpcHBvLTY4LmNsZXJrLmFjY291bnRzLmRldiQ";
--- packages/auth/src/auth-options.ts
+++ packages/auth/src/auth-options.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { type DefaultSession, type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
 
 import { prisma } from "@acme/db";
 
@@ -45,6 +46,10 @@ export const authOptions: NextAuthOptions = {
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID as string,
       clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
     /**
      * ...add more providers here
