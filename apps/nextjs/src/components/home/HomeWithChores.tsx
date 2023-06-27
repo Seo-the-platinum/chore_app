3a8d326b05201c3b1b024f027383621839d1669b
Eliseo Flores <eliseo_flores530@yahoo.com>
2023-06-27 16:49:25 -0700

    styling changes on profile page

--- apps/nextjs/src/components/home/HomeWithChores.tsx
+++ apps/nextjs/src/components/home/HomeWithChores.tsx
@@ -4,23 +4,24 @@ import Link from 'next/link'
 import type { HomeWithProps } from '../../types/components/HomeWith'
 
 const HomeWithChores = ({ home }: HomeWithProps) => {
-
     return (
-        <div className='divide-y-2 divide-emerald-400 text-center border-2 border-slate-50 rounded-lg py-2 px-1'>
-            <h3 className='text-2xl pb-2'>{home.name}</h3>
-            <ul className='text-start p-2'>
-                {
-                    home.chores && home.chores.map((chore) => {
-                        return (
-                            <li key={chore.id}>
-                                <Link className=' rounded-sm bg-gradient-to-tr from-purple-800 to-indigo-800 p-1' href={`/chores/${chore.id}`}>
-                                    {chore.title}
-                                </Link>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+        <div className='bg-gradient-to-br from-indigo-900 to-fuchsia-800 rounded-lg'>
+            <div className='divide-y-2 divide-emerald-400 text-center rounded-lg bg-slate-900 m-[3px] p-2'>
+                <h3 className='text-2xl pb-2'>{home.name}</h3>
+                <ul className='text-start p-2 pt-4'>
+                    {
+                        home.chores && home.chores.map((chore) => {
+                            return (
+                                <li className='bg-gradient-to-br from-red-800 to-blue-800 flex text-center rounded-lg' key={chore.id}>
+                                    <Link className='rounded-lg bg-slate-900 m-[2px] w-full' href={`/chores/${chore.id}`}>
+                                        {chore.title}
+                                    </Link>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
         </div>
     )
 }
--- apps/nextjs/src/components/users/Profile.tsx
+++ apps/nextjs/src/components/users/Profile.tsx
@@ -6,10 +6,16 @@ import type { ProfileProps } from '../../types/components/profile'
 
 const Profile = ({ user }: ProfileProps) => {
     return (
-        <div className='flex flex-col divide-y-2 divide-indigo-700'>
-            <div className='flex items-end gap-4 pb-4'>
-                <Image alt='Users Profile Image' className='rounded-full' src={`${user.image ? user.image : '/images/temp.png'}`} width={84} height={84} />
-                <h2 className='text-2xl'>{user.username}</h2>
+        <div className='flex flex-col'>
+            <div className='
+                flex flex-col gap-4 pb-4 
+                after:bg-gradient-to-br after:from-emerald-500 
+                after:to-indigo-900 after:w-full 
+                after:h-1 after:rounded-lg after:brightness-150'>
+                <div className="flex items-end">
+                    <Image alt='Users Profile Image' className='rounded-full' src={`${user.image ? user.image : '/images/temp.png'}`} width={84} height={84} />
+                    <h2 className='text-2xl'>{user.username}</h2>
+                </div>
             </div>
             <div className='pt-4 text-center flex-col flex gap-2'>
                 <h2 className='text-3xl'>Homes</h2>
@@ -21,7 +27,7 @@ const Profile = ({ user }: ProfileProps) => {
                     }
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
