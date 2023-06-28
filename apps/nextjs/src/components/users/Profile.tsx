5dc0f372ef74dfe1670427984810ce0f6abc3c22
Eliseo Flores <eliseo_flores530@yahoo.com>
2023-06-27 17:00:17 -0700

    padding and sizing adjustments

--- apps/nextjs/src/components/users/Profile.tsx
+++ apps/nextjs/src/components/users/Profile.tsx
@@ -6,13 +6,13 @@ import type { ProfileProps } from '../../types/components/profile'
 
 const Profile = ({ user }: ProfileProps) => {
     return (
-        <div className='flex flex-col'>
+        <div className='flex flex-col w-full px-2'>
             <div className='
                 flex flex-col gap-4 pb-4 
                 after:bg-gradient-to-br after:from-emerald-500 
                 after:to-indigo-900 after:w-full 
                 after:h-1 after:rounded-lg after:brightness-150'>
-                <div className="flex items-end">
+                <div className="flex items-end justify-center">
                     <Image alt='Users Profile Image' className='rounded-full' src={`${user.image ? user.image : '/images/temp.png'}`} width={84} height={84} />
                     <h2 className='text-2xl'>{user.username}</h2>
                 </div>
--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -20,7 +20,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex flex-col items-center text-white">
-        <div className="container flex flex-col items-center justify-center gap-4 px-4">
+        <div className="container flex flex-col items-center justify-center gap-4">
           <h1 className="text-4xl font-extrabold tracking-tight sm:text-[5rem]">
             Chore <span className='text-emerald-500'>App</span>
           </h1>
