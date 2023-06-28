8d6f9cbb04179728e7f0cd6dae9b4b6f7250ee34
Eliseo Flores <eliseo_flores530@yahoo.com>
2023-06-27 17:44:14 -0700

    more styling changes

--- apps/nextjs/src/components/home/HomeWithChores.tsx
+++ apps/nextjs/src/components/home/HomeWithChores.tsx
@@ -5,7 +5,7 @@ import type { HomeWithProps } from '../../types/components/HomeWith'
 
 const HomeWithChores = ({ home }: HomeWithProps) => {
     return (
-        <div className='bg-gradient-to-br from-indigo-900 to-fuchsia-800 rounded-lg'>
+        <div className='bg-gradient-to-br from-indigo-900 to-fuchsia-800 rounded-lg w-64'>
             <div className='divide-y-2 divide-emerald-400 text-center rounded-lg bg-slate-900 m-[3px] p-2'>
                 <h3 className='text-2xl pb-2'>{home.name}</h3>
                 <ul className='text-start p-2 pt-4'>
--- apps/nextjs/src/components/users/Profile.tsx
+++ apps/nextjs/src/components/users/Profile.tsx
@@ -6,20 +6,20 @@ import type { ProfileProps } from '../../types/components/profile'
 
 const Profile = ({ user }: ProfileProps) => {
     return (
-        <div className='flex flex-col w-full px-2'>
+        <div className='flex flex-col px-2 items-center'>
             <div className='
-                flex flex-col gap-4 pb-4 
+                flex flex-col gap-4 pb-4 w-3/4
                 after:bg-gradient-to-br after:from-emerald-500 
-                after:to-indigo-900 after:w-full 
-                after:h-1 after:rounded-lg after:brightness-150'>
-                <div className="flex items-end justify-center">
+                after:to-indigo-900 after:h-1 after:rounded-lg 
+                after:brightness-150'>
+                <div className="flex items-end justify-evenly">
                     <Image alt='Users Profile Image' className='rounded-full' src={`${user.image ? user.image : '/images/temp.png'}`} width={84} height={84} />
                     <h2 className='text-2xl'>{user.username}</h2>
                 </div>
             </div>
-            <div className='pt-4 text-center flex-col flex gap-2'>
+            <div className='pt-4 flex-col flex gap-2 items-center'>
                 <h2 className='text-3xl'>Homes</h2>
-                <div className='flex flex-col gap-8'>
+                <div className='flex flex-col gap-8 items-center'>
                     {
                         user.homes && user.homes.map((home) => {
                             return <HomeWithChores key={home.id} home={home} />
